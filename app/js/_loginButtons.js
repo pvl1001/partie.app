993642deed64a3040a74b2e08511558ec7fa9298
meloman1001 <meloman1001@mail.ru>
2020-08-10 13:43:10 +0300

    2075

--- app/js/_loginButtons.js
+++ app/js/_loginButtons.js
@@ -2,13 +2,10 @@ let arrShowAll = document.querySelectorAll('#showAll')
 let arrBgBtnContainerShow = document.querySelectorAll('.bg-btn-container-show')
 let arrBtnSocial = document.querySelectorAll('#btnSocial')
 
-
 for (let i = 0; i < arrShowAll.length; i++) {
     let showAll = arrShowAll[i]
     let bgBtnContainerShow = arrBgBtnContainerShow[i]
     let btnSocial = arrBtnSocial[i]
-
-    console.log(arrBgBtnContainerShow)
 
     function showAllBtns() { // анимация кнопок на стартовой
         bgBtnContainerShow.style.cssText = 'opacity: 0.6; z-index: 0;'
