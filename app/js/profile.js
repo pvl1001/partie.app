9b7061a4fe9a9c631c5e20670e2709cebc0e9cca
meloman1001 <meloman1001@mail.ru>
2020-08-20 16:07:27 +0300

    2075

--- app/js/profile.js
+++ app/js/profile.js
@@ -3,13 +3,13 @@ function openTab(evt, Tab) {
     var i, tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.querySelectorAll(".profile__tabs .tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.querySelectorAll(".profile__tabs .tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
@@ -17,6 +17,59 @@ function openTab(evt, Tab) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(Tab).style.display = "block";
     evt.currentTarget.className += " active";
+
 }
-let defaultOpen = document.getElementById("defaultOpen");
-if(defaultOpen !== null) defaultOpen.click()
+
+function openTabModal(evt, Tab) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.querySelectorAll("#levelProgress .tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.querySelectorAll("#levelProgress .tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(Tab).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    let token = document.querySelectorAll( '.pop' )
+    let popovers = document.querySelectorAll( '.popover' )
+    for (let i = 0; i < token.length; i++) {
+        let el = token[i]
+        let popover = popovers[i]
+        let h = popover.getBoundingClientRect().height
+        let x = el.getBoundingClientRect().left - 92
+        let y = el.getBoundingClientRect().top - 20 - h
+        el.addEventListener( 'mouseover', function () {
+            popover.style.opacity = '1'
+            popover.style.visibility = 'visible'
+            popover.style.left = x + 'px'
+            popover.style.top = y + 'px'
+        } )
+        el.addEventListener( 'mouseout', function () {
+            popover.style.cssText = ''
+        } )
+    }
+}
+
+let defaultOpen = document.getElementById("defaultOpen")
+if(defaultOpen !== null) {
+    defaultOpen.click()
+}
+
+let defaultOpen2 = document.getElementById("defaultOpen2")
+if(defaultOpen2 !== null) defaultOpen2.click()
+
+
+
+
+
+
--- dist/js/script.js
+++ dist/js/script.js
@@ -12,6 +12,9 @@ let subscriptionModal = document.querySelector('#subscriptionModal')
 let newPost = document.querySelector('#newPost')
 let chooseGame = document.querySelector('#chooseGame')
 let profileProgress = document.querySelector('#profileProgress')
+let levelProgress = document.querySelector('#levelProgress')
+let applyPartner = document.querySelector('#applyPartner')
+let editProfile = document.querySelector('#editProfile')
 let arrlogInMenu = document.querySelectorAll('.logIn__menu')
 let arrModalWrapp = document.querySelectorAll( '.modal-wrapp' )
 let arrFeedHeaderMenu = document.querySelectorAll( '.feed__header_menu' )
@@ -430,13 +433,13 @@ function openTab(evt, Tab) {
     var i, tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.querySelectorAll(".profile__tabs .tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.querySelectorAll(".profile__tabs .tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
@@ -444,7 +447,60 @@ function openTab(evt, Tab) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(Tab).style.display = "block";
     evt.currentTarget.className += " active";
+
 }
-let defaultOpen = document.getElementById("defaultOpen");
-if(defaultOpen !== null) defaultOpen.click()
+
+function openTabModal(evt, Tab) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.querySelectorAll("#levelProgress .tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.querySelectorAll("#levelProgress .tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(Tab).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    let token = document.querySelectorAll( '.pop' )
+    let popovers = document.querySelectorAll( '.popover' )
+    for (let i = 0; i < token.length; i++) {
+        let el = token[i]
+        let popover = popovers[i]
+        let h = popover.getBoundingClientRect().height
+        let x = el.getBoundingClientRect().left - 92
+        let y = el.getBoundingClientRect().top - 20 - h
+        el.addEventListener( 'mouseover', function () {
+            popover.style.opacity = '1'
+            popover.style.visibility = 'visible'
+            popover.style.left = x + 'px'
+            popover.style.top = y + 'px'
+        } )
+        el.addEventListener( 'mouseout', function () {
+            popover.style.cssText = ''
+        } )
+    }
+}
+
+let defaultOpen = document.getElementById("defaultOpen")
+if(defaultOpen !== null) {
+    defaultOpen.click()
+}
+
+let defaultOpen2 = document.getElementById("defaultOpen2")
+if(defaultOpen2 !== null) defaultOpen2.click()
+
+
+
+
+
+
 
