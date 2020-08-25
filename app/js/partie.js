c7947a2cdbf370fe0d12931af9c2d3b656f7135d
meloman1001 <meloman1001@mail.ru>
2020-08-25 17:53:12 +0300

    2075

--- app/js/partie.js
+++ app/js/partie.js
@@ -10,3 +10,79 @@ window.addEventListener('scroll', () => {
     }
 });
 
+
+let arrUser = document.querySelectorAll('.message__user_user-name')
+let arrWindow = document.querySelectorAll('.message .user-window')
+let arrClosePlace = document.querySelectorAll('.user-window .close-place')
+
+for(let i = 0; i < arrUser.length; i++){
+    let user = arrUser[i]
+    let window = arrWindow[i]
+    let closePlace = arrClosePlace[i]
+    user.addEventListener('click', function () {
+        window.style.opacity = '1'
+        window.style.visibility = 'visible'
+    })
+    closePlace.addEventListener('click', function () {
+        window.style.opacity = ''
+        window.style.visibility = ''
+    })
+}
+
+function openMore() {
+    let moreWin = document.querySelector( '.more__win' )
+    let closePlaceMore = document.querySelector( '.big-round-btn.more .close-place' )
+    moreWin.style.visibility = 'visible'
+    moreWin.style.opacity = '1'
+    closePlaceMore.style.display = 'block'
+    closePlaceMore.addEventListener( 'click', function (e) {
+        moreWin.style.cssText = ''
+        closePlaceMore.style.display = ''
+        e.stopPropagation()
+    } )
+}
+
+
+let onClickStart
+function clickStart() {
+    onClickStart = !onClickStart
+    let btnStart = document.querySelector( '.big-round-btn.start' )
+
+    if (onClickStart) {
+        btnStart.style.backgroundColor = '#F45365'
+        btnStart.parentNode.children[1].innerHTML = 'End Partie'
+    } else {
+        btnStart.style.cssText = ''
+        btnStart.parentNode.children[1].innerHTML = 'Start'
+    }
+}
+
+
+let arrReportInput = document.querySelectorAll('#partieReport input')
+let reportBtn = document.querySelector('#partieReport button')
+for(let input of arrReportInput) {
+    input.addEventListener('change', function () {
+        reportBtn.removeAttribute('disabled')
+        reportBtn.classList.remove('disabled')
+        reportBtn.classList.add('bg-fiolet')
+    })
+}
+
+
+function reportSubmitted() {
+    let reportSubmitted = document.querySelector('.reportSubmitted')
+    closeModal(partieReport)
+    reportSubmitted.style.visibility = 'visible'
+    reportSubmitted.style.opacity = '1'
+    setTimeout(function () {
+        reportSubmitted.style.cssText = ''
+    },5000)
+}
+
+
+let arrStar = document.querySelectorAll('.rating__stars input')
+for(let star of arrStar){
+    star.addEventListener('change', function () {
+        if(star.checked) console.log(this.checked)
+    })
+}
--- dist/js/script.js
+++ dist/js/script.js
@@ -22,6 +22,11 @@ let gametragPlatform = document.querySelector('#gametragPlatform')
 let addGametrag = document.querySelector('#addGametrag')
 let hostLiveShow = document.querySelector('#hostLiveShow')
 let additionalSettings = document.querySelector('#additionalSettings')
+let removeUser = document.querySelector('#removeUser')
+let titleModal = document.querySelector('#titleModal')
+let editPartie = document.querySelector('#editPartie')
+let partieReport = document.querySelector('#partieReport')
+let titleModalStart = document.querySelector('#titleModalStart')
 let arrHeaderMenuMenu = document.querySelectorAll('.header__menu_menu')
 let arrlogInMenu = document.querySelectorAll('.logIn__menu')
 let arrModalWrapp = document.querySelectorAll( '.modal-wrapp' )
@@ -633,4 +638,80 @@ window.addEventListener('scroll', () => {
     }
 });
 
+
+let arrUser = document.querySelectorAll('.message__user_user-name')
+let arrWindow = document.querySelectorAll('.message .user-window')
+let arrClosePlace = document.querySelectorAll('.user-window .close-place')
+
+for(let i = 0; i < arrUser.length; i++){
+    let user = arrUser[i]
+    let window = arrWindow[i]
+    let closePlace = arrClosePlace[i]
+    user.addEventListener('click', function () {
+        window.style.opacity = '1'
+        window.style.visibility = 'visible'
+    })
+    closePlace.addEventListener('click', function () {
+        window.style.opacity = ''
+        window.style.visibility = ''
+    })
+}
+
+function openMore() {
+    let moreWin = document.querySelector( '.more__win' )
+    let closePlaceMore = document.querySelector( '.big-round-btn.more .close-place' )
+    moreWin.style.visibility = 'visible'
+    moreWin.style.opacity = '1'
+    closePlaceMore.style.display = 'block'
+    closePlaceMore.addEventListener( 'click', function (e) {
+        moreWin.style.cssText = ''
+        closePlaceMore.style.display = ''
+        e.stopPropagation()
+    } )
+}
+
+
+let onClickStart
+function clickStart() {
+    onClickStart = !onClickStart
+    let btnStart = document.querySelector( '.big-round-btn.start' )
+
+    if (onClickStart) {
+        btnStart.style.backgroundColor = '#F45365'
+        btnStart.parentNode.children[1].innerHTML = 'End Partie'
+    } else {
+        btnStart.style.cssText = ''
+        btnStart.parentNode.children[1].innerHTML = 'Start'
+    }
+}
+
+
+let arrReportInput = document.querySelectorAll('#partieReport input')
+let reportBtn = document.querySelector('#partieReport button')
+for(let input of arrReportInput) {
+    input.addEventListener('change', function () {
+        reportBtn.removeAttribute('disabled')
+        reportBtn.classList.remove('disabled')
+        reportBtn.classList.add('bg-fiolet')
+    })
+}
+
+
+function reportSubmitted() {
+    let reportSubmitted = document.querySelector('.reportSubmitted')
+    closeModal(partieReport)
+    reportSubmitted.style.visibility = 'visible'
+    reportSubmitted.style.opacity = '1'
+    setTimeout(function () {
+        reportSubmitted.style.cssText = ''
+    },5000)
+}
+
+
+let arrStar = document.querySelectorAll('.rating__stars input')
+for(let star of arrStar){
+    star.addEventListener('change', function () {
+        if(star.checked) console.log(this.checked)
+    })
+}
 
