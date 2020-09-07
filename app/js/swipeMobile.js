21e55a2b84e1fe11baf1abf030a0b02acdf95dde
meloman1001 <meloman1001@mail.ru>
2020-09-07 21:50:25 +0300

    2075

--- app/js/swipeMobile.js
+++ app/js/swipeMobile.js
@@ -105,7 +105,7 @@ if(window.innerWidth <= 1023) {
                 shortcutsUp()
             } else {
                 /* down swipe */
-                shortcutsUp()
+                shortcutsDown()
             }
         }
         /* reset values */
@@ -114,7 +114,119 @@ if(window.innerWidth <= 1023) {
     }
 
 
+
+
+    let postMenus = document.querySelectorAll( '.post-menu.mobile' )
+    for(let postMenuMenu of postMenus) {
+        if (postMenuMenu !== null) {
+            postMenuMenu.children[0].addEventListener( 'touchstart', handleTouchStart, false );
+            postMenuMenu.children[0].addEventListener( 'touchmove', handleTouchMove, false );
+        }
+
+        var xDown = null;
+        var yDown = null;
+
+        function getTouches(evt) {
+            return evt.touches ||             // browser API
+                evt.originalEvent.touches; // jQuery
+        }
+
+        function handleTouchStart(evt) {
+            const firstTouch = getTouches( evt )[0];
+            xDown = firstTouch.clientX;
+            yDown = firstTouch.clientY;
+        }
+
+        function handleTouchMove(evt) {
+            if (!xDown || !yDown) {
+                return;
+            }
+            var xUp = evt.touches[0].clientX;
+            var yUp = evt.touches[0].clientY;
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+
+            if (Math.abs( xDiff ) > Math.abs( yDiff )) {/*most significant*/
+                if (xDiff > 0) { /* left swipe */
+                } else {/* right swipe */
+                }
+            } else {
+                if (yDiff > 0) {
+                    /* up swipe */
+
+                } else {
+                    /* down swipe */
+                    postMenuMenu.style.cssText = ''
+                    postMenuMenu.children[0].style.cssText = ''
+                }
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;
+        }
+    }
+
+
+
+
+    let btnContainerWrapps = document.querySelectorAll( '.btn-container-wrapp' )
+    for(let el of btnContainerWrapps) {
+        if (el !== null) {
+            console.log(el)
+            el.children[0].addEventListener( 'touchstart', handleTouchStart, false );
+            el.children[0].addEventListener( 'touchmove', handleTouchMove, false );
+        }
+
+        var xDown = null;
+        var yDown = null;
+
+        function getTouches(evt) {
+            return evt.touches ||             // browser API
+                evt.originalEvent.touches; // jQuery
+        }
+
+        function handleTouchStart(evt) {
+            const firstTouch = getTouches( evt )[0];
+            xDown = firstTouch.clientX;
+            yDown = firstTouch.clientY;
+        }
+
+        function handleTouchMove(evt) {
+            if (!xDown || !yDown) {
+                return;
+            }
+            var xUp = evt.touches[0].clientX;
+            var yUp = evt.touches[0].clientY;
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+
+            if (Math.abs( xDiff ) > Math.abs( yDiff )) {/*most significant*/
+                if (xDiff > 0) { /* left swipe */
+                } else {/* right swipe */
+                }
+            } else {
+                if (yDiff > 0) {
+                    /* up swipe */
+
+                } else {
+                    /* down swipe */
+                    el.style.top = ''
+                    el.children[0].classList.add('animate__fadeOutDownBig')
+                    el.children[0].classList.remove('animate__fadeInUp')
+                }
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;
+        }
+    }
+
+
+
+
+
     let modals = document.querySelectorAll( '.modal' )
+
     for (let modal of modals) {
         if (modal.parentElement !== hostLiveShowMobile &&
             modal.parentElement !== hostPartie &&
@@ -130,6 +242,7 @@ if(window.innerWidth <= 1023) {
             modal.parentElement !== welcomeMobile &&
             modal.parentElement !== newPost &&
             modal.parentElement !== preferencesModal &&
+            modal.parentElement !== titleModal &&
             modal.parentElement !== editShortcutMobile) {
 
             modal.addEventListener( 'touchstart', handleTouchStart, false );
