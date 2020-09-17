3bce704ca0502c7b9405985c3ea7ce6928f55f8b
meloman1001 <meloman1001@mail.ru>
2020-09-17 19:50:06 +0300

    2075

--- app/js/swipeMobile.js
+++ app/js/swipeMobile.js
@@ -62,54 +62,54 @@ if(window.innerWidth <= 1023) {
 if(window.innerWidth <= 1023) {
 
 
-    let shortcutsMobile = document.querySelector( '#hostPartie .shortcuts' )
-
-    if (shortcutsMobile !== null) {
-        shortcutsMobile.addEventListener( 'touchstart', handleTouchStart, false );
-        shortcutsMobile.addEventListener( 'touchmove', handleTouchMove, false );
-    }
-
-    var xDown = null;
-    var yDown = null;
-
-    function getTouches(evt) {
-        return evt.touches ||             // browser API
-            evt.originalEvent.touches; // jQuery
-    }
-
-    function handleTouchStart(evt) {
-        const firstTouch = getTouches( evt )[0];
-        xDown = firstTouch.clientX;
-        yDown = firstTouch.clientY;
-    }
-
-    function handleTouchMove(evt) {
-        if (!xDown || !yDown) {
-            return;
-        }
-        var xUp = evt.touches[0].clientX;
-        var yUp = evt.touches[0].clientY;
-        var xDiff = xDown - xUp;
-        var yDiff = yDown - yUp;
-
-        if (Math.abs( xDiff ) > Math.abs( yDiff )) {/*most significant*/
-            if (xDiff > 0) { /* left swipe */
-            } else {/* right swipe */
-            }
-        } else {
-            if (yDiff > 0) {
-                /* up swipe */
-                shortcutsUp()
-            } else {
-                /* down swipe */
-                shortcutsDown()
-            }
-        }
-        /* reset values */
-        xDown = null;
-        yDown = null;
-    }
-
+    // let shortcutsMobile = document.querySelector( '#hostPartie .shortcuts' )
+    //
+    // if (shortcutsMobile !== null) {
+    //     shortcutsMobile.addEventListener( 'touchstart', handleTouchStart, false );
+    //     shortcutsMobile.addEventListener( 'touchmove', handleTouchMove, false );
+    // }
+    //
+    // var xDown = null;
+    // var yDown = null;
+    //
+    // function getTouches(evt) {
+    //     return evt.touches ||             // browser API
+    //         evt.originalEvent.touches; // jQuery
+    // }
+    //
+    // function handleTouchStart(evt) {
+    //     const firstTouch = getTouches( evt )[0];
+    //     xDown = firstTouch.clientX;
+    //     yDown = firstTouch.clientY;
+    // }
+    //
+    // function handleTouchMove(evt) {
+    //     if (!xDown || !yDown) {
+    //         return;
+    //     }
+    //     var xUp = evt.touches[0].clientX;
+    //     var yUp = evt.touches[0].clientY;
+    //     var xDiff = xDown - xUp;
+    //     var yDiff = yDown - yUp;
+    //
+    //     if (Math.abs( xDiff ) > Math.abs( yDiff )) {/*most significant*/
+    //         if (xDiff > 0) { /* left swipe */
+    //         } else {/* right swipe */
+    //         }
+    //     } else {
+    //         if (yDiff > 0) {
+    //             /* up swipe */
+    //             shortcutsUp()
+    //         } else {
+    //             /* down swipe */
+    //             shortcutsDown()
+    //         }
+    //     }
+    //     /* reset values */
+    //     xDown = null;
+    //     yDown = null;
+    // }
+    //
 
 
 
