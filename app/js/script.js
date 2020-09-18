f692d66602edd23fde05d844de231670766f7403
Igor Vetrov <ceo@igorvetrov.ru>
2020-09-19 00:39:06 +0300

    fix click ios

--- app/js/script.js
+++ app/js/script.js
@@ -378,8 +378,8 @@ if(shortcuts !== null) {
                 }
             } else {
                 hostPartie.children[0].style.overflow = 'hidden'
-                // shortcuts.style.bottom = '0'
-                // btnUp.style.transform = 'rotateX(190deg)'
+                shortcuts.style.bottom = '0'
+                btnUp.style.transform = 'rotateX(190deg)'
                 // bgClick.style.zIndex = '1'
                 // if (window.innerWidth >= 1024) bgClick.style.opacity = '1'
             }
