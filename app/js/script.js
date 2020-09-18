3b0b4b52ef0c3bc1791d7ffd183c8bab49f5e539
meloman1001 <meloman1001@mail.ru>
2020-09-19 00:49:20 +0300

    2075

--- app/js/script.js
+++ app/js/script.js
@@ -379,7 +379,7 @@ if(shortcuts !== null) {
             } else {
                 hostPartie.children[0].style.overflow = 'hidden'
                 shortcuts.style.bottom = '0'
-                // btnUp.style.transform = 'rotateX(190deg)'
+                btnUp.style.transform = 'rotateX(190deg)'
                 // bgClick.style.zIndex = '1'
                 // if (window.innerWidth >= 1024) bgClick.style.opacity = '1'
             }
