fd71d42480fafb3cc1a30b6a924c4101df0573dd
meloman1001 <meloman1001@mail.ru>
2020-09-19 13:05:34 +0300

    2075

--- app/js/swipeMobile.js
+++ app/js/swipeMobile.js
@@ -1,119 +1,68 @@
+// if (window.innerWidth <= 1023) {
+//
+//     let profileProgressModal = document.querySelector( '#profileProgress .modal' )
+//     if (profileProgressModal !== null) {
+//         profileProgressModal.addEventListener( 'touchstart', handleTouchStart, false );
+//         profileProgressModal.addEventListener( 'touchmove', handleTouchMove, false );
+//     }
+//
+//     var xDown = null;
+//     var yDown = null;
+//
+//     function getTouches(evt) {
+//         return evt.touches ||             // browser API
+//             evt.originalEvent.touches; // jQuery
+//     }
+//
+//     function handleTouchStart(evt) {
+//         const firstTouch = getTouches( evt )[0];
+//         xDown = firstTouch.clientX;
+//         yDown = firstTouch.clientY;
+//     }
+//
+//     function handleTouchMove(evt) {
+//         if (!xDown || !yDown) {
+//             return;
+//         }
+//         var xUp = evt.touches[0].clientX;
+//         var yUp = evt.touches[0].clientY;
+//         var xDiff = xDown - xUp;
+//         var yDiff = yDown - yUp;
+//
+//         if (Math.abs( xDiff ) > Math.abs( yDiff )) {/*most significant*/
+//             if (xDiff > 0) { /* left swipe */
+//             } else {/* right swipe */
+//             }
+//         } else {
+//             if (yDiff > 0) {
+//                 /* up swipe */
+//                 profileProgressModal.children[2].style.height = 'calc(100vh - 154px)'
+//                 profileProgressModal.style.backgroundColor = '#17171a'
+//                 profileProgressModal.classList.add( 'modal-arrow' )
+//                 setTimeout( function () {
+//                     profileProgressModal.children[2].style.overflow = 'auto'
+//                 }, 500 )
+//
+//             } else {
+//                 /* down swipe */
+//                 if (profileProgressModal.offsetTop !== 0) closeModal( profileProgress )
+//                 if (window.innerWidth <= 1023) {
+//                     if (profileProgressModal.children[2].scrollTop === 0) {
+//                         profileProgressModal.children[2].style.cssText = ''
+//                         profileProgressModal.style.backgroundColor = ''
+//                         profileProgressModal.classList.remove( 'modal-arrow' )
+//                     }
+//                 }
+//             }
+//         }
+//         /* reset values */
+//         xDown = null;
+//         yDown = null;
+//     }
+// }
+
+
 if (window.innerWidth <= 1023) {
-
-    let profileProgressModal = document.querySelector( '#profileProgress .modal' )
-    if (profileProgressModal !== null) {
-        profileProgressModal.addEventListener( 'touchstart', handleTouchStart, false );
-        profileProgressModal.addEventListener( 'touchmove', handleTouchMove, false );
-    }
-
-    var xDown = null;
-    var yDown = null;
-
-    function getTouches(evt) {
-        return evt.touches ||             // browser API
-            evt.originalEvent.touches; // jQuery
-    }
-
-    function handleTouchStart(evt) {
-        const firstTouch = getTouches( evt )[0];
-        xDown = firstTouch.clientX;
-        yDown = firstTouch.clientY;
-    }
-
-    function handleTouchMove(evt) {
-        if (!xDown || !yDown) {
-            return;
-        }
-        var xUp = evt.touches[0].clientX;
-        var yUp = evt.touches[0].clientY;
-        var xDiff = xDown - xUp;
-        var yDiff = yDown - yUp;
-
-        if (Math.abs( xDiff ) > Math.abs( yDiff )) {/*most significant*/
-            if (xDiff > 0) { /* left swipe */
-            } else {/* right swipe */
-            }
-        } else {
-            if (yDiff > 0) {
-                /* up swipe */
-                profileProgressModal.children[2].style.height = 'calc(100vh - 154px)'
-                profileProgressModal.style.backgroundColor = '#17171a'
-                profileProgressModal.classList.add( 'modal-arrow' )
-                setTimeout( function () {
-                    profileProgressModal.children[2].style.overflow = 'auto'
-                }, 500 )
-
-            } else {
-                /* down swipe */
-                if (profileProgressModal.offsetTop !== 0) closeModal( profileProgress )
-                if (window.innerWidth <= 1023) {
-                    if (profileProgressModal.children[2].scrollTop === 0) {
-                        profileProgressModal.children[2].style.cssText = ''
-                        profileProgressModal.style.backgroundColor = ''
-                        profileProgressModal.classList.remove( 'modal-arrow' )
-                    }
-                }
-            }
-        }
-        /* reset values */
-        xDown = null;
-        yDown = null;
-    }
-}
-
-
-if (window.innerWidth <= 1023) {
-
-
-    // let shortcutsMobile = document.querySelector( '#hostPartie .shortcuts' )
-    //
-    // if (shortcutsMobile !== null) {
-    //     shortcutsMobile.addEventListener( 'touchstart', handleTouchStart, false );
-    //     shortcutsMobile.addEventListener( 'touchmove', handleTouchMove, false );
-    // }
-    //
-    // var xDown = null;
-    // var yDown = null;
-    //
-    // function getTouches(evt) {
-    //     return evt.touches ||             // browser API
-    //         evt.originalEvent.touches; // jQuery
-    // }
-    //
-    // function handleTouchStart(evt) {
-    //     const firstTouch = getTouches( evt )[0];
-    //     xDown = firstTouch.clientX;
-    //     yDown = firstTouch.clientY;
-    // }
-    //
-    // function handleTouchMove(evt) {
-    //     if (!xDown || !yDown) {
-    //         return;
-    //     }
-    //     var xUp = evt.touches[0].clientX;
-    //     var yUp = evt.touches[0].clientY;
-    //     var xDiff = xDown - xUp;
-    //     var yDiff = yDown - yUp;
-    //
-    //     if (Math.abs( xDiff ) > Math.abs( yDiff )) {/*most significant*/
-    //         if (xDiff > 0) { /* left swipe */
-    //         } else {/* right swipe */
-    //         }
-    //     } else {
-    //         if (yDiff > 0) {
-    //             /* up swipe */
-    //             shortcutsUp()
-    //         } else {
-    //             /* down swipe */
-    //             shortcutsDown()
-    //         }
-    //     }
-    //     /* reset values */
-    //     xDown = null;
-    //     yDown = null;
-    // }
-    //
-
 
     let postMenus = document.querySelectorAll( '.post-menu.mobile' )
     for (let postMenuMenu of postMenus) {
@@ -295,6 +244,35 @@ $( function () {
                 shortcutsUp()
             } else if (direction === 'down') {
                 shortcutsDown()
+            }
+        }
+    } );
+
+} );
+
+
+
+let profileProgressModal = document.querySelector( '#profileProgress .modal' )
+$( function () {
+    $( "#profileProgress .modal" ).swipe( {
+        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+
+            if (direction === 'up') {
+                profileProgressModal.children[2].style.height = 'calc(100vh - 154px)'
+                profileProgressModal.style.backgroundColor = '#17171a'
+                profileProgressModal.classList.add( 'modal-arrow' )
+                setTimeout( function () {
+                    profileProgressModal.children[2].style.overflow = 'auto'
+                }, 500 )
+            } else if (direction === 'down') {
+                if (profileProgressModal.offsetTop !== 0) closeModal( profileProgress )
+                if (window.innerWidth <= 1023) {
+                    if (profileProgressModal.children[2].scrollTop === 0) {
+                        profileProgressModal.children[2].style.cssText = ''
+                        profileProgressModal.style.backgroundColor = ''
+                        profileProgressModal.classList.remove( 'modal-arrow' )
+                    }
+                }
             }
         }
     } );
